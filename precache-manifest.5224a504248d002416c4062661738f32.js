f7d6ee906a44dfd85b8e7cbaa943ce7dcb814805
USER <user@USERs-MacBook-Pro.local>
2020-03-05 05:49:25 -0500

    Updates

---
+++ precache-manifest.5224a504248d002416c4062661738f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c041236ced2ba48bc06f3ad71a43adf0",
+    "url": "/Quiz/static/media/background.c041236c.png"
+  },
+  {
+    "revision": "f22d5387155722563924",
+    "url": "/Quiz/static/js/runtime~main.f22d5387.js"
+  },
+  {
+    "revision": "9f390eea9d90c8496f81",
+    "url": "/Quiz/static/js/main.9f390eea.chunk.js"
+  },
+  {
+    "revision": "e60eca116bd6fb4529bb",
+    "url": "/Quiz/static/js/1.e60eca11.chunk.js"
+  },
+  {
+    "revision": "9f390eea9d90c8496f81",
+    "url": "/Quiz/static/css/main.bcca6ae6.chunk.css"
+  },
+  {
+    "revision": "3372b04eea4b86c1d28c5be611233751",
+    "url": "/Quiz/index.html"
+  }
+];
